c007443cf10533de7c52113b2c3620ac128d682e
Tijana Djokovic <tdjokovic00@gmail.com>
2022-11-05 00:37:13 +0100

    SearchLocation fixed
    Locations are fetched and loaded.
    *search bar is not visible - to be fixed!

--- CheckPoint/FrontEnd/app/src/main/java/com/example/frontend/activities/SearchLocationActivity.kt
+++ CheckPoint/FrontEnd/app/src/main/java/com/example/frontend/activities/SearchLocationActivity.kt
@@ -1,79 +1,126 @@
 package com.example.frontend.activities
 
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.MeasurePolicy
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.frontend.api.CustomCallback
 import com.example.frontend.api.Requests
 import com.example.frontend.models.LocationDTO
 
-class SearchLocationActivity:AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        val Lista:List<LocationDTO> = Requests.search("E");
-        setContent {
-                lista(Lista);
-        }
-    }
-}
 
 @Composable
-fun lista(Lista:List<LocationDTO>){
-    LazyColumn{
-        itemsIndexed(Lista){index: Int, item: LocationDTO ->
-            Text(text = item.name)
-        }
-    }
-}
+fun LocationScreen(navController: NavController)
+{
+    var mList: List<LocationDTO> by remember {  mutableStateOf (listOf()) }
 
-
-@Composable
-fun search() {
-    var searchText by remember{ mutableStateOf("")}
-    val focusManager = LocalFocusManager.current
-    var locationList:List<LocationDTO> = emptyList()
     Column(
-
-        Modifier
+        modifier = Modifier
             .navigationBarsPadding()
-            .padding(24.dp)
             .fillMaxSize()
-    ) {
-        TextField(
-            value = searchText,
-            label = {
-                Text("Search location")
-            },
-            onValueChange = {
-                searchText = it
-                locationList = Requests.search(searchText)
-            },
-            singleLine = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
+            .padding(20.dp)
+    ){
+        search(mList = mList);
+
+        Divider(
+            color = Color.DarkGray,
+            thickness = 2.dp,
+            modifier = Modifier.padding(vertical = 30.dp)
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            modifier = Modifier.height(50.dp),
-            onClick = {
-                locationList = Requests.search(searchText)
-        }){
-            Text("Search")
+
+        lista(mList = mList);
+    }
+
+    Requests.getAll(object : CustomCallback {
+        override fun onSucess(locList: List<LocationDTO>) {
+            Log.d("CALLBACK", locList.toString());
+
+//            locationList = locList;
+            mList = locList;
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        locationList.forEach { loc ->
-            Text(loc.name)
+        override fun onFailure() {}
+    })
+}
+
+@Composable
+fun lista(
+    mList : List<LocationDTO>
+){
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ){
+        items(mList){
+                each -> locationCard(location = each)
         }
     }
+}
+
+@Composable
+fun locationCard(location : LocationDTO)
+{
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        elevation = 2.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp))
+    ){
+        Row {
+            Column (
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = location.name)
+                Text(text = location.coordinateX.toString())
+                Text(text = location.coordinateY.toString())
+            }
+        }
+    }
+}
+
+
+@Composable
+fun search(
+    mList : List<LocationDTO>
+) {
+    var list by remember {
+        mutableStateOf(mList);
+    }
+
+    var searchText by remember{ mutableStateOf("")}
+//    val focusManager = LocalFocusManager.current
+//    var locationList:List<LocationDTO> = emptyList()
+
+
+    OutlinedTextField(
+        value = searchText,
+        leadingIcon = {
+                      Icon(imageVector = Icons.Filled.Search, contentDescription = "Search Icon")
+        },
+        onValueChange = {
+                searchText = it
+                Requests.search( it , object : CustomCallback {
+                    override fun onSucess(locList: List<LocationDTO>) {
+                        list = locList;
+                    }
+
+                    override fun onFailure() {}
+                })
+            },
+        modifier = Modifier.fillMaxWidth(),
+    )
 }
