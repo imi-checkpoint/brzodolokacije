62e8e22944bcccd8702ba13c01b31080523c48b8
Sonja Galovic <galovicsonja@gmail.com>
2022-11-09 23:51:26 +0100

    Dodate metode za prebrojavanje pratilaca i osoba koje korisnik prati. Izmenjeno i prepakovano par metoda.

--- CheckPoint/BackEnd/src/main/java/imi/spring/backend/controllers/FollowersController.java
+++ CheckPoint/BackEnd/src/main/java/imi/spring/backend/controllers/FollowersController.java
@@ -2,6 +2,7 @@ package imi.spring.backend.controllers;
 
 import imi.spring.backend.models.AppUser;
 import imi.spring.backend.services.FollowersService;
+import imi.spring.backend.services.JWTService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -17,25 +18,40 @@ import java.util.List;
 @RequestMapping(path = "/follow_list")
 public class FollowersController {
     private final FollowersService followersService;
+    private final JWTService jwtService;
 
     /* Koga sve dati korisnik prati? */
-    @GetMapping("/following")
+    @GetMapping("/following/{userId}")
     @ResponseBody
-    public List<AppUser> getAllFollowingByUser(HttpServletRequest request) throws ServletException {
-        return followersService.getAllFollowingByUser(request);
+    public List<AppUser> getAllFollowingByUser(@PathVariable Long userId) {
+        return followersService.getAllFollowingByUser(userId);
     }
 
     /* Ko sve prati datog korisnika? */
-    @GetMapping("/followers")
+    @GetMapping("/followers/{userId}")
     @ResponseBody
-    public List<AppUser> getAllFollowersPerUser(HttpServletRequest request) throws ServletException {
-        return followersService.getAllFollowersPerUser(request);
+    public List<AppUser> getAllFollowersPerUser(@PathVariable Long userId) {
+        return followersService.getAllFollowersPerUser(userId);
     }
 
     @PostMapping("/follow_or_unfollow/{userId}")
     @ResponseBody
     public String followOrUnfollowUser(HttpServletRequest request, @PathVariable Long userId) throws ServletException {
-        return followersService.followOrUnfollowUser(request, userId);
+        AppUser userFromJWT = jwtService.getAppUserFromJWT(request);
+        if (userFromJWT != null)
+            return followersService.followOrUnfollowUser(userFromJWT.getId(), userId);
+        return "Invalid user!";
     }
 
+    @GetMapping("/following/count/{userId}")
+    @ResponseBody
+    public Integer countAllFollowingByUser(@PathVariable Long userId) {
+        return followersService.countAllFollowingByUser(userId);
+    }
+
+    @GetMapping("/followers/count/{userId}")
+    @ResponseBody
+    public Integer countAllFollowersPerUser(@PathVariable Long userId) {
+        return followersService.countAllFollowersPerUser(userId);
+    }
 }
