ce44845b86a060367cffb408c14fe6c9342578e7
Filip Kocovic <fkocovic@gmail.com>
2022-12-05 18:14:49 +0000

    Revert "Revert "Fixed server bug that wouldnt register users with default photo because of...""
    This reverts commit cc17c380d1742cab68fc10b609c1b9c0d4823e65

--- CheckPoint/BackEnd/src/main/java/imi/spring/backend/BackendApplication.java
+++ CheckPoint/BackEnd/src/main/java/imi/spring/backend/BackendApplication.java
@@ -9,13 +9,20 @@ import imi.spring.backend.services.FollowersService;
 import imi.spring.backend.services.LocationService;
 import imi.spring.backend.services.PostService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
@@ -23,6 +30,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 		SecurityFilterAutoConfiguration.class}
 )
 public class BackendApplication {
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 
@@ -38,10 +48,27 @@ public class BackendApplication {
 	@Bean
 	CommandLineRunner run(AppUserService appUserService, LocationService locationService, PostService postService, FollowersService followersService){
 		return args -> {
-			appUserService.saveUser(new AppUser("user1@gmail.com", "user1", "user1"));
-			appUserService.saveUser(new AppUser("user2@gmail.com", "user2", "user2"));
-			appUserService.saveUser(new AppUser("user3@gmail.com", "user3", "user3"));
-			appUserService.saveUser(new AppUser("user4@gmail.com", "user4", "user4"));
+
+
+			System.out.println(env.getProperty("profile.image.default"));
+
+			byte[] pictureInBytes = Files.readAllBytes(Path.of(env.getProperty("profile.image.default")));
+
+			AppUser user = new AppUser("user1@gmail.com", "user1", "user1");
+			user.setImage(pictureInBytes);
+			appUserService.saveUser(user);
+
+			user = new AppUser("user2@gmail.com", "user2", "user2");
+			user.setImage(pictureInBytes);
+			appUserService.saveUser(user);
+
+			user = new AppUser("user3@gmail.com", "user3", "user3");
+			user.setImage(pictureInBytes);
+			appUserService.saveUser(user);
+
+			user = new AppUser("user4@gmail.com", "user4", "user4");
+			user.setImage(pictureInBytes);
+			appUserService.saveUser(user);
 
 			locationService.saveLocation(new Location("Eiffel Tower", 48.858093, 2.294694));
 			locationService.saveLocation(new Location("Stonehenge", 51.1740, -1.8224));
--- CheckPoint/BackEnd/src/main/java/imi/spring/backend/controllers/AppUserController.java
+++ CheckPoint/BackEnd/src/main/java/imi/spring/backend/controllers/AppUserController.java
@@ -6,6 +6,7 @@ import imi.spring.backend.services.AppUserService;
 import imi.spring.backend.services.JWTService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
@@ -33,6 +34,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 public class AppUserController {
     private final AppUserService appUserService;
     private final JWTService jwtService;
+    private final Environment env;
 
     @GetMapping("/users")
     public ResponseEntity<List<AppUser>> getAllUsers(){
@@ -74,8 +76,7 @@ public class AppUserController {
             appUser.setImage(profileImage.getBytes());
         }
         else {
-            appUser.setImage(Files.readAllBytes(Path.of("src" + File.separator+ "main" + File.separator+ "resources" + File.separator +
-                    "static" + File.separator+ "images" + File.separator+ "default-user.jpeg")));
+            appUser.setImage(Files.readAllBytes(Path.of(env.getProperty("profile.image.default"))));
         }
 
         try {
