d40ef5575c9573a4fbf5f140e7b211f632130178
Sonja Galovic <galovicsonja@gmail.com>
2022-12-11 01:08:20 +0100

    Zavrsena PostCard komponenta, dodat image slider.

--- CheckPoint/FrontEnd/app/src/main/java/com/example/frontend/presentation/posts/PostsScreen.kt
+++ CheckPoint/FrontEnd/app/src/main/java/com/example/frontend/presentation/posts/PostsScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,10 +30,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.frontend.domain.model.Photo
 import com.example.frontend.domain.model.Post
@@ -41,9 +46,11 @@ import com.example.frontend.presentation.destinations.MainLocationScreenDestinat
 import com.example.frontend.presentation.destinations.PostScreenDestination
 import com.example.frontend.presentation.destinations.ProfileScreenDestination
 import com.example.frontend.presentation.posts.components.PostStringState
+import com.google.accompanist.pager.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import java.util.Base64
+import kotlin.math.absoluteValue
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Destination
@@ -149,7 +156,7 @@ fun AllPosts(
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
 @Composable
 fun PostCard(
     post : Post,
@@ -159,11 +166,10 @@ fun PostCard(
 )
 {
     Card(
-        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-        shape = RoundedCornerShape(corner = CornerSize(6.dp)),
-        elevation = 2.dp,
+        modifier = Modifier.padding(vertical = 8.dp),
+        elevation = 1.dp,
         backgroundColor = MaterialTheme.colorScheme.surface,
-        onClick ={
+        onClick = {
             navigator.navigate(PostScreenDestination(post.postId))
         }
     ){
@@ -210,14 +216,14 @@ fun PostCard(
                                 .padding(horizontal = 12.dp, vertical = 4.dp),
                             verticalArrangement = Arrangement.Center
                         ) {
+
                             Text(
                                 text = post.appUserUsername,
                                 style = MaterialTheme.typography.bodyLarge
                             )
                             Text(
-                                text = post.date,
+                                text = post.location.name,
                                 style = MaterialTheme.typography.bodySmall,
-                                fontSize = 11.sp,
                                 color = MaterialTheme.colorScheme.outline
                             )
                         }
@@ -228,23 +234,27 @@ fun PostCard(
                 }
 
                 Row(){
-                    if(post.photos.size > 0){
-                        Spacer(modifier = Modifier.height(5.dp))
-                        PhotoCard(photo = post.photos[0], navigator)
-                    }
+                    if(post.photos.size > 0)
+                        ImagePagerSliderPostCard(post, post.photos)
                 }
-
-                Spacer(modifier = Modifier.height(5.dp))
+                Spacer(modifier = Modifier.height(6.dp))
                 Text(
-                    text = post.description,
+                    text = post.date,
+                    style = MaterialTheme.typography.bodySmall,
+                    fontSize = 10.sp,
+                    fontStyle = FontStyle.Italic,
+                    color = MaterialTheme.colorScheme.outline
+                )
+                Text(
+                    text = if (post.description.length < 90) post.description else "${post.description.substring(0, 85)} ...",
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onSurfaceVariant,
                 )
-
+                Spacer(modifier = Modifier.height(5.dp))
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Row (
                         verticalAlignment = Alignment.CenterVertically
@@ -262,38 +272,104 @@ fun PostCard(
                         )
                     }
                 }
+                /*Box(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = post.date,
+                        style = MaterialTheme.typography.bodySmall,
+                        fontSize = 10.sp,
+                        fontStyle = FontStyle.Italic,
+                        color = MaterialTheme.colorScheme.outline,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }*/
             }
         }
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
+@ExperimentalPagerApi
 @Composable
-fun PhotoCard(
-    photo : Photo,
-    navigator: DestinationsNavigator,
+fun ImagePagerSliderPostCard(
+    post: Post,
+    photos: List<Photo>
 ){
-    Row(
+    val pagerState = rememberPagerState(
+        pageCount = photos.size,
+        initialPage = 0
+    )
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        println(photo.photo.data.toByteArray().size)
-        val decoder = Base64.getDecoder()
-        val photoBytes = decoder.decode(photo.photo.data)
-        if(photoBytes.size>1){
-            val mapa:Bitmap = BitmapFactory.decodeByteArray(photoBytes,0,photoBytes.size)
-            print(mapa.byteCount)
-            if(mapa!=null){
-                Image(
-                    bitmap = mapa.asImageBitmap(),
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                )
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+        ){
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(0.dp, 15.dp, 0.dp, 15.dp)
+            ) {
+                    page->
+                Card(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                            lerp(
+                                start = 0.85f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
+                        }
+                        .fillMaxWidth()
+                ){
+
+                    val photo = photos[page]
+                    val decoder = Base64.getDecoder()
+                    val photoBytes = decoder.decode(photo.photo.data)
+                    if(photoBytes.size>1) {
+                        val mapa: Bitmap =
+                            BitmapFactory.decodeByteArray(photoBytes, 0, photoBytes.size)
+                        print(mapa.byteCount)
+                        if (mapa != null) {
+                            Image(
+                                bitmap = mapa.asImageBitmap(),
+                                contentDescription = "",
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
             }
         }
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            indicatorWidth = 6.dp,
+            indicatorHeight = 6.dp,
+            activeColor = Color.Blue,
+            inactiveColor = Color.LightGray
+        )
     }
 }
 
